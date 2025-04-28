7df1c29c33f8ea49835f94f15591c04b6fe840bf
Nikhil <nikhil.thakur@softradix.in>
2025-04-28 19:02:27 +0530

    changed Login page

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -11,14 +11,14 @@ const Layout = () => {
 
       <div className="relative h-full w-full">
         <img src={bgImg} alt="" />
-        <div className="absolute h-full w-full md:top-[85%] xl:right-[13%]  lg:right-[17%] md:right-[32%]  flex items-center justify-center flex-col ">
+        <div className="absolute h-full w-full md:top-[85%] xl:right-[13%]  right-[28%] lg:right-[17%] md:right-[32%]  flex items-center justify-center flex-col ">
           <div className=" flex md:ml-[80px] h-full gap-2">
             <img className="h-[40px] w-[40px]" src={logo} alt="" />
-            <h1 className="text-4xl ">Hope UI</h1>
+            <h1 className="text-2xl md:text-4xl ">Hope UI</h1>
           </div>
           <div className=" flex flex-col ml-[250px] mt-[4%] w-full items-center justify-center ">
-          <h1 className="font-semibold text-4xl ">Sign In</h1>
-          <h6 className="font-lighter text-[18px] text-[#8A92A6] tracking-wider mt-[20px]">Sign in to stay connected.</h6>
+          <h1 className="font-semibold text-2xl md:text-4xl ">Sign In</h1>
+          <h6 className="font-lighter text-sm md:text-[18px] text-[#8A92A6] tracking-wider mt-[20px]">Sign in to stay connected.</h6>
           <SignInField/>
         
         </div>
